ae4fdbe8a75babf4d601c42d5c84c75a1912322e
gxherror <xherror@foxmail.com>
2022-12-14 01:51:01 +0800

    server enable image post

--- server/server.go
+++ server/server.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"os"
+	"path"
 	"server/models"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/items", getItems)
+	router.GET("/images/:imageName", getImage)
 	router.GET("/items/:category", getItem)
 	router.POST("/items", addItem)
+	router.POST("/images", addImage)
+	//router.DELETE()
+	//router.PUT()
 	router.Run()
 }
 
@@ -44,5 +51,38 @@ func addItem(c *gin.Context) {
 	} else {
 		models.AddItem(prod)
 		c.IndentedJSON(http.StatusCreated, prod)
+	}
+}
+
+func getImage(c *gin.Context) {
+	//https://www.jianshu.com/p/6950550b2937
+	//fileDir := c.Query("fileDir")
+	//fileName := c.Query("fileName")
+	imageName := c.Param("imageName")
+	//打开文件
+	_, errByOpenFile := os.Open(path.Join("./images", imageName))
+	if errByOpenFile != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
+	c.Header("Content-Type", "application/octet-stream")
+	c.Header("Content-Disposition", "attachment; filename="+fileName)
+	c.Header("Content-Transfer-Encoding", "binary")
+	c.File(fileDir + "/" + fileName)
+	return
+}
+
+func addImage(c *gin.Context) {
+	form, err := c.MultipartForm()
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+	files := form.File["image"]
+	//https://blog.csdn.net/lvjie13450/article/details/123164878
+	for _, file := range files {
+		dest := path.Join("./images/", file.Filename)
+		err := c.SaveUploadedFile(file, dest)
+		if err != nil {
+			fmt.Println("Err", err.Error())
+		}
 	}
 }
